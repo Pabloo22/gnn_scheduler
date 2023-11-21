2bc9b55dd954e828f5a3af4473d99e05e2b860ce
Pabloo22 <pablete.arino@gmail.com>
2023-11-21 10:20:01 +0000

    Add get_project_path function to utils.py

---
+++ gnn_scheduler/utils.py
@@ -0,0 +1,19 @@
+import pathlib
+import os
+
+import dotenv  # pylint: disable=import-error
+
+
+def get_project_path() -> pathlib.Path:
+    """Gets project path."""
+    dotenv.load_dotenv()
+    project_path = os.getenv("PROJECT_PATH")
+
+    if project_path is None:
+        raise ValueError("PROJECT_PATH environment variable not set.")
+
+    return pathlib.Path(project_path)
+
+
+if __name__ == "__main__":
+    print(get_project_path())
