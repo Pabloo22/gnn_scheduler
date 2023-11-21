ee3212ec4f8eebce174e58487c85f7983467ec47
Pabloo22 <pablete.arino@gmail.com>
2023-11-21 20:20:17 +0000

    Update `__init__.py` files

---
+++ gnn_scheduler/__init__.py
@@ -0,0 +1,1 @@
+from .utils import get_project_path
--- gnn_scheduler/jssp/__init__.py
+++ gnn_scheduler/jssp/__init__.py
@@ -1,1 +1,5 @@
 from .job_shop_instance import Operation, JobShopInstance
+from .load_utils import (load_from_file,
+                         load_from_benchmark, 
+                         load_metadata, 
+                         load_all_from_benchmark)
