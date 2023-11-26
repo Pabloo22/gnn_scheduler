6978b7d1241d4d778addc8b48c1363e4fab0745f
Pabloo22 <pablete.arino@gmail.com>
2023-11-26 20:26:26 +0000

    Add preprocessing functions for graph data

---
+++ gnn_scheduler/data/preprocessing/node_feature_creators.py
@@ -0,0 +1,192 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+import networkx as nx
+
+from gnn_scheduler.data.preprocessing import get_n_jobs, get_n_machines
+
+
+class NodeFeatureCreator(ABC):
+    """Base class for node feature creators."""
+
+    def __init__(self):
+        self.graph = None
+        self.is_fit = False
+        self.name = self.__class__.__name__
+
+    def fit(self, graph: nx.DiGraph):
+        """Used to fit the feature creator to the graph.
+
+        Stores the graph.
+
+        It can be implemented by sub-classes if necessary to store more
+        information.
+        """
+        self.graph = graph
+        self.is_fit = True
+
+    @abstractmethod
+    def create_features(self, node_name: str, node_data: dict[str, Any]) -> list[float]:
+        """_summary_
+
+        Args:
+            node_name (str): name of the node
+            node_data (dict[str, Any]): data associated with the node
+
+        Returns:
+            list[float]: a list of floats representing the features of the node
+        """
+
+    def __str__(self) -> str:
+        return f"{self.name}()"
+
+    def __call__(self, node_name: str, node_data: dict[str, Any]) -> list[float]:
+        if not self.is_fit:
+            raise RuntimeError(f"{self.name} is not fit.")
+
+        return self.create_features(node_name, node_data)
+
+
+class InAndOutDegrees(NodeFeatureCreator):
+    """The normalized in- and out-degrees of a node."""
+
+    def fit(self, graph: nx.DiGraph):
+        """Stores the graph"""
+        self.graph = graph
+
+    def create_node_features(
+        self, node_name: str, node_data: dict[str, Any]
+    ) -> list[float]:
+        """Returns the normalized in- and out-degrees of a node.
+
+        Args:
+            node_name (str): name of the node
+            node_data (dict[str, Any]): data associated with the node
+            graph (nx.DiGraph): the networkx graph
+
+        Returns:
+            list[float]:
+        """
+        in_degree = self.graph.in_degree(node_name) / (self.graph.number_of_nodes() - 1)
+        out_degree = self.graph.out_degree(node_name) / (
+            self.graph.number_of_nodes() - 1
+        )
+        return [in_degree, out_degree]
+
+
+class OneHotEncoding(NodeFeatureCreator):
+    """One-hot encoding of a node attribute.
+
+    Args:
+        NodeFeatureCreator (_type_):
+    """
+
+    def __init__(self, feature_name: str, n_values: int = 100):
+        super().__init__()
+        self.feature_name = feature_name
+        self.n_values = n_values
+
+    def create_node_fetures(
+        self, node_name: str, node_data: dict[str, Any]
+    ) -> list[float]:
+        """Creates the one-hot encoding of the node attribute.
+
+        Args:
+            node_name (str):
+            node_data (dict[str, Any]):
+            graph (nx.DiGraph):
+
+        Returns:
+            list[float]:
+        """
+        zeros = [0.0] * self.n_values
+        machine_id = node_data.get(self.feature_name)
+        if machine_id is not None and 0 <= machine_id < self.n_values:
+            zeros[machine_id] = 1.0
+        return zeros
+
+
+class Duration(NodeFeatureCreator):
+    """The processing time required for each operation.
+
+    It is normalized by the maximum operation time across the graph to ensure
+    this feature falls within a consistent range."""
+
+    def __init__(self):
+        super().__init__()
+        self.max_duration = 0.0
+
+    def fit(self, graph: nx.DiGraph):
+        """Calculates the maximum operation time across the graph."""
+        for node in graph.nodes:
+            self.max_duration = max(self.max_duration, graph.nodes[node]["duration"])
+        self.is_fit = True
+
+    def create_features(self, node_name: str, node_data: dict[str, Any]) -> list[float]:
+        return [node_data["duration"] / self.max_duration]
+
+
+class MachineLoad(NodeFeatureCreator):
+    """How utilized each machine is.
+
+    A calculation of the total processing time that is scheduled for each
+    machine, normalized by the maximum load across all machines, to provide a
+    sense of how utilized each machine is.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.max_load = 0
+        self.machines_load = None
+
+    def fit(self, graph: nx.DiGraph):
+        """Calculates the maximum load across all machines."""
+        self.graph = graph
+        machines_load = [0] * get_n_machines(graph)
+        for _, node_data in graph.nodes(data=True):
+            machines_load[node_data["machine_id"]] += node_data["duration"]
+
+        self.machines_load = machines_load
+        self.max_load = max(machines_load)
+        self.is_fit = True
+
+    def create_features(self, node_name: str, node_data: dict[str, Any]) -> list[float]:
+        machine_id = node_data["machine_id"]
+        return [self.machines_load[machine_id] / self.max_load]
+
+
+class OperationIndex(NodeFeatureCreator):
+    """The index of the operation in the job.
+
+    The sequential position of an operation within its job, represented as a
+    fraction of the total number of operations in the job, to give the model
+    insight into the operation's order without depending on the absolute
+    number of operations.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.n_operations_per_job = None
+
+    def fit(self, graph: nx.DiGraph):
+        """Calculates the number of operations per job."""
+        self.graph = graph
+        n_operations_per_job = [0] * get_n_jobs(graph)
+        for _, node_data in graph.nodes(data=True):
+            n_operations_per_job[node_data["job_id"]] += 1
+        
+        self.n_operations_per_job = n_operations_per_job
+        self.is_fit = True
+    
+    def create_features(self, node_name: str, node_data: dict[str, Any]) -> list[float]:
+        job_id = node_data["job_id"]
+        position = node_data["position"] + 1
+        return [position / self.n_operations_per_job[job_id]]
+    
+    
+
+
+if __name__ == "__main__":
+    pass
---
+++ gnn_scheduler/data/preprocessing/preprocessing_pipeline.py
@@ -0,0 +1,122 @@
+from __future__ import annotations
+
+from typing import Optional
+
+import networkx as nx
+
+from gnn_scheduler.data.preprocessing import NodeFeatureCreator
+
+
+def preprocess_graph(
+    graph: nx.DiGraph,
+    node_feature_creators: list[NodeFeatureCreator],
+    new_feature_name: str = "x",
+    keep_old_features: bool = False,
+    exclude_old_features: Optional[list[str]] = None,
+    copy: bool = False,
+    remove_nodes: Optional[list[str]] = None,
+) -> nx.DiGraph:
+    """Preprocesses a graph using a list of node feature creators.
+    
+    It creates a new feature for each node using the node feature creators.
+    The new feature is stored in the node data under the name new_feature_name.
+    If keep_old_features is True, the old features are kept. Otherwise, they 
+    are removed. If exclude_old_features is not None, the old features to 
+    exclude are specified.
+
+    Args:
+        graph (nx.DiGraph): the graph to preprocess
+        node_feature_creators (list[NodeFeatureCreator]): the node feature
+            creators to use.
+        new_feature_name (str, optional): the name of the new feature. Defaults
+            to "x".
+        keep_old_features (bool, optional): whether to keep the old features.
+            Defaults to False.
+        exclude_old_features (Optional[list[str]], optional): the old features
+            to exclude if keep_old_features is False. Defaults to None.
+        copy (bool, optional): whether to copy the graph before preprocessing.
+            Defaults to False.
+        remove_nodes (Optional[list[str]], optional): the nodes names to 
+            remove. Defaults to [].
+
+    Returns:
+        nx.DiGraph: the preprocessed graph
+    """
+
+    if copy:
+        graph = graph.copy()
+
+    remove_nodes = [] if remove_nodes is None else remove_nodes
+
+    for node_name in remove_nodes:
+        graph.remove_node(node_name)
+
+    exclude_old_features = [] if exclude_old_features is None else exclude_old_features
+    exclude_old_features = set(exclude_old_features)
+    exclude_old_features.add(new_feature_name)
+
+    # Fit the node feature creators
+    for node_feature_creator in node_feature_creators:
+        node_feature_creator.fit(graph)
+
+    for node_name, node_data in graph.nodes(data=True):
+        # Add the new feature
+        features = []
+        for node_feature_creator in node_feature_creators:
+            features.extend(node_feature_creator(node_name, node_data))
+        node_data[new_feature_name] = features
+
+        # Remove old features if necessary
+        if keep_old_features:
+            continue
+        for feature_name in node_data.copy():
+            if feature_name in exclude_old_features:
+                continue
+            del node_data[feature_name]
+
+    return graph
+
+
+def preprocess_graphs(
+    graphs: list[nx.DiGraph],
+    node_feature_creators: list[NodeFeatureCreator],
+    new_feature_name: str = "x",
+    keep_old_features: bool = False,
+    exclude_old_features: Optional[list[str]] = None,
+    copy: bool = False,
+    remove_nodes: Optional[list[str]] = None,
+) -> list[nx.DiGraph]:
+    """Preprocesses a list of graphs using a list of node feature creators.
+
+    Args:
+        graphs (list[nx.DiGraph]): the graphs to preprocess
+        node_feature_creators (list[NodeFeatureCreator]): the node feature
+            creators to use.
+        new_feature_name (str, optional): the name of the new feature. Defaults
+            to "x".
+        keep_old_features (bool, optional): whether to keep the old features.
+            Defaults to False.
+        exclude_old_features (Optional[list[str]], optional): the old features
+            to exclude if keep_old_features is False. Defaults to None.
+        copy (bool, optional): whether to copy the graph before preprocessing.
+            Defaults to False.
+        remove_nodes (Optional[list[str]], optional): the nodes names to
+            remove. Defaults to [].
+
+    Returns:
+        list[nx.DiGraph]: the preprocessed graphs
+    """
+    processed_graphs = []
+    for graph in graphs:
+        processed_graphs.append(
+            preprocess_graph(
+                graph,
+                node_feature_creators=node_feature_creators,
+                new_feature_name=new_feature_name,
+                keep_old_features=keep_old_features,
+                exclude_old_features=exclude_old_features,
+                copy=copy,
+                remove_nodes=remove_nodes,
+            )
+        )
+    return processed_graphs
