f9caef9482801d8211eb55fdc27605382a4e2e92
Pabloo22 <pablete.arino@gmail.com>
2023-12-03 21:22:58 +0000

    Fix and add more control to NaiveGenerator

--- gnn_scheduler/jssp/generators/naive_generator.py
+++ gnn_scheduler/jssp/generators/naive_generator.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import random
+from typing import Optional
 
 from gnn_scheduler.jssp import JobShopInstance, Operation
 
@@ -12,29 +13,36 @@ class NaiveGenerator:
         max_n_machines: int,
         max_duration: int,
         name_suffix: str = "naive_generated_instance",
+        seed: Optional[int] = None,
     ):
         self.max_n_jobs = max_n_jobs
         self.max_n_machines = max_n_machines
         self.max_duration = max_duration
         self.name_suffix = name_suffix
         self.counter = 0
+        self.seed = seed
+        random.seed(self.seed)
 
-    def generate(self) -> JobShopInstance:
-        n_jobs = random.randint(1, self.max_n_jobs)
-        n_machines = random.randint(1, self.max_n_machines)
+    def generate(
+        self, n_jobs: Optional[int] = None, n_machines: Optional[int] = None
+    ) -> JobShopInstance:
+        if n_jobs is None:
+            n_jobs = random.randint(2, self.max_n_jobs)
+        if n_machines is None:
+            n_machines = random.randint(n_jobs, self.max_n_machines)
         jobs = []
         available_machines = list(range(n_machines))
         for _ in range(n_jobs):
             operations = []
             for _ in range(n_machines):
                 machine_id = random.choice(available_machines)
-                # remove machine from available machines
                 available_machines.remove(machine_id)
                 duration = random.randint(1, self.max_duration)
                 operations.append(
                     Operation(machine_id=machine_id, duration=duration)
                 )
             jobs.append(operations)
+            available_machines = list(range(n_machines))
 
         return JobShopInstance(jobs=jobs, name=self._get_name())
 
