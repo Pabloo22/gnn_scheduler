08aa4842b58280bf2f24eac985ebb744dd1494b8
Pabloo22 <pablete.arino@gmail.com>
2023-12-07 20:31:02 +0000

    Refactor NaiveGenerator class to include minimum duration and minimum number of jobs

--- gnn_scheduler/jssp/generators/naive_generator.py
+++ gnn_scheduler/jssp/generators/naive_generator.py
@@ -9,15 +9,19 @@ from gnn_scheduler.jssp import JobShopInstance, Operation
 class NaiveGenerator:
     def __init__(
         self,
-        max_n_jobs: int,
-        max_n_machines: int,
         max_duration: int,
+        min_duration: int = 1,
+        max_n_jobs: int = 20,
+        min_n_jobs: int = 2,
+        max_n_machines: int = 10,
         name_suffix: str = "naive_generated_instance",
         seed: Optional[int] = None,
     ):
         self.max_n_jobs = max_n_jobs
         self.max_n_machines = max_n_machines
         self.max_duration = max_duration
+        self.min_duration = min_duration
+        self.min_n_jobs = min_n_jobs
         self.name_suffix = name_suffix
         self.counter = 0
         self.seed = seed
@@ -27,7 +31,7 @@ class NaiveGenerator:
         self, n_jobs: Optional[int] = None, n_machines: Optional[int] = None
     ) -> JobShopInstance:
         if n_jobs is None:
-            n_jobs = random.randint(2, self.max_n_jobs)
+            n_jobs = random.randint(self.min_n_jobs, self.max_n_jobs)
         if n_machines is None:
             n_machines = random.randint(n_jobs, self.max_n_machines)
         jobs = []
@@ -37,7 +41,7 @@ class NaiveGenerator:
             for _ in range(n_machines):
                 machine_id = random.choice(available_machines)
                 available_machines.remove(machine_id)
-                duration = random.randint(1, self.max_duration)
+                duration = random.randint(self.min_duration, self.max_duration)
                 operations.append(
                     Operation(machine_id=machine_id, duration=duration)
                 )
