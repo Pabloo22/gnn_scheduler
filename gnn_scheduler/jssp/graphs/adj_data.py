e338aa76b532726e14059c6520844d1273cbae53
Pabloo22 <pablete.arino@gmail.com>
2023-12-08 21:02:50 +0000

    Add target attribute to AdjData class

--- gnn_scheduler/jssp/graphs/adj_data.py
+++ gnn_scheduler/jssp/graphs/adj_data.py
@@ -8,9 +8,16 @@ class AdjData(NamedTuple):
 
     adj_matrix: torch.Tensor
     node_features: torch.Tensor
+    target: torch.Tensor | float
 
     def __repr__(self) -> str:
+        target = (
+            self.target
+            if isinstance(self.target, float)
+            else self.target.size()
+        )
         return (
             f"AdjData(adj_matrix={self.adj_matrix.size()}, "
-            f"node_features={self.node_features.size()})"
+            f"node_features={self.node_features.size()}, "
+            f"target={target})"
         )
