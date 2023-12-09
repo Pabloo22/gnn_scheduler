2ac27014f94e85705b9e4a9c507bcd756b732ef5
Pabloo22 <pablete.arino@gmail.com>
2023-12-09 17:12:14 +0000

    Add train_eval_test_split

--- gnn_scheduler/gnns/__init__.py
+++ gnn_scheduler/gnns/__init__.py
@@ -6,6 +6,5 @@ from gnn_scheduler.gnns.models import (
     RelationalGCNRegressor,
 )
 from gnn_scheduler.gnns.training import (
-    train_eval_test_split_by_name,
-    train_test_split_by_name,
+    train_eval_test_split,
 )
--- gnn_scheduler/gnns/training/__init__.py
+++ gnn_scheduler/gnns/training/__init__.py
@@ -1,4 +1,5 @@
 from .data_utils import (
+    train_eval_test_split,
     train_eval_test_split_by_name,
     train_test_split_by_name,
     get_names_with_n_machines,
