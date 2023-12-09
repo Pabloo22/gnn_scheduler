c0463133ac63e0e5c9275225bc1c0295c0251549
Pabloo22 <pablete.arino@gmail.com>
2023-12-09 15:45:35 +0000

    Add import statements for training functions

---
+++ gnn_scheduler/gnns/__init__.py
@@ -0,0 +1,11 @@
+from gnn_scheduler.gnns.models import (
+    GraphAggregationLayer,
+    GraphConvolutionLayer,
+    GraphConvolution,
+    MultiGraphConvolutionLayers,
+    RelationalGCNRegressor,
+)
+from gnn_scheduler.gnns.training import (
+    train_eval_test_split_by_name,
+    train_test_split_by_name,
+)
---
+++ gnn_scheduler/gnns/training/__init__.py
@@ -0,0 +1,5 @@
+from .data_utils import (
+    train_eval_test_split_by_name,
+    train_test_split_by_name,
+    get_names_with_n_machines,
+)
