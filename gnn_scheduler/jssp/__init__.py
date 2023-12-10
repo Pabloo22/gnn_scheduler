79721abab16a5b281978a540d71f3feb583b549e
Pabloo22 <pablete.arino@gmail.com>
2023-12-10 21:21:32 +0000

    Add load_pickle_object and load_pickle_objects_from_folders functions to __init__

--- gnn_scheduler/jssp/__init__.py
+++ gnn_scheduler/jssp/__init__.py
@@ -5,5 +5,7 @@ from .load_utils import (
     load_metadata,
     load_all_from_benchmark,
     load_pickle_instances,
-    load_pickle_instances_from_folders
+    load_pickle_instances_from_folders,
+    load_pickle_object,
+    load_pickle_objects_from_folders,
 )
