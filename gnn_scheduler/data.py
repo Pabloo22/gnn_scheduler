8063cfe19d0c5ddf639a8250ef4fe6b2e01a866e
Pabloo22 <pablete.arino@gmail.com>
2025-03-05 13:54:13 +0100

    Add JobShopDataset

--- gnn_scheduler/data.py
+++ gnn_scheduler/data.py
@@ -1,5 +1,42 @@
+import os
+import json
+import random
+from typing import Type
+from collections.abc import Sequence
+import numpy as np
 import torch
-from torch_geometric.data import HeteroData  # type: ignore[import-untyped]
+from torch_geometric.data import (  # type: ignore[import-untyped]
+    HeteroData,
+    InMemoryDataset,
+    download_url,
+)
+import tqdm  # type: ignore[import-untyped]
+
+from job_shop_lib.dispatching.feature_observers import (
+    FeatureObserverType,
+    FeatureObserver,
+    FeatureObserverConfig,
+)
+from job_shop_lib.graphs import build_resource_task_graph
+from job_shop_lib.reinforcement_learning import (
+    SingleJobShopGraphEnv,
+    ResourceTaskGraphObservation,
+    get_optimal_actions,
+    ResourceTaskGraphObservationDict,
+)
+from job_shop_lib.dispatching import OptimalOperationsObserver
+from job_shop_lib import Schedule
+
+from gnn_scheduler.utils import get_data_path
+
+
+_DEFAULT_FEATURE_OBSERVERS_TYPES = [
+    FeatureObserverType.DURATION,
+    FeatureObserverType.EARLIEST_START_TIME,
+    FeatureObserverType.IS_SCHEDULED,
+    FeatureObserverType.POSITION_IN_JOB,
+    FeatureObserverType.REMAINING_OPERATIONS,
+]
 
 
 class JobShopData(HeteroData):
@@ -21,3 +58,212 @@ class JobShopData(HeteroData):
             return increments
 
         return super().__inc__(key, value, *args, **kwargs)
+
+
+class JobShopDataset(InMemoryDataset):
+
+    def __init__(
+        self,
+        root: str = str(get_data_path()),
+        transform=None,
+        pre_transform=None,
+        processed_filename: str = "job_shop_data.pt",
+        raw_filename: str = "small_random_instances_0.json",
+        feature_observers_types: (
+            Sequence[
+                str
+                | FeatureObserverType
+                | Type[FeatureObserver]
+                | FeatureObserverConfig
+            ]
+            | None
+        ) = None,
+    ):
+        self.feature_observers_types = (
+            feature_observers_types
+            if feature_observers_types is not None
+            else _DEFAULT_FEATURE_OBSERVERS_TYPES
+        )
+        self.processed_filename = processed_filename
+        self.raw_filename = raw_filename
+        super().__init__(root, transform, pre_transform)
+        self.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self) -> list[str]:
+        return [os.path.join(self.raw_dir, self.raw_filename)]
+
+    @property
+    def processed_file_names(self) -> list[str]:
+        return [os.path.join(self.processed_dir, self.processed_filename)]
+
+    def download(self):
+        if os.path.exists(self.raw_paths[0]):
+            return
+        url = (
+            "https://github.com/Pabloo22/gnn_scheduler/blob/main/data/raw/"
+            + self.raw_filename
+        )
+        download_url(url, self.raw_dir)
+
+    def process(self):
+        schedules_json = self._load_schedules()
+        observations, action_probabilities_sequence = (
+            self.get_observation_action_pairs(
+                schedules_json, self.feature_observers_types
+            )
+        )
+        # Save intermediate results
+        with open(
+            self.processed_dir + "/observations.json", "w", encoding="utf-8"
+        ) as f:
+            json.dump(observations, f)
+        with open(
+            self.processed_dir + "/action_probabilities.json",
+            "w",
+            encoding="utf-8",
+        ) as f:
+            json.dump(action_probabilities_sequence, f)
+
+        hetero_dataset = self.process_observation_action_pairs(
+            observations, action_probabilities_sequence
+        )
+        self.save(hetero_dataset, path=self.processed_paths[0])
+
+    def _load_schedules(self):
+        schedules_json = []
+        for raw_path in self.raw_paths:
+            with open(raw_path, "r", encoding="utf-8") as f:
+                schedules_json.extend(json.load(f))
+        return schedules_json
+
+    @staticmethod
+    def process_observation_action_pairs(
+        observations: list[ResourceTaskGraphObservationDict],
+        action_probabilities_sequence: list[dict[tuple[int, int, int], float]],
+    ) -> list[JobShopData]:
+        hetero_dataset: list[JobShopData] = []
+        for obs, action_probs in tqdm.tqdm(
+            zip(observations, action_probabilities_sequence),
+            desc="Processing observation-action pairs",
+        ):
+            job_shop_data = JobShopData()
+            for key, value in obs.items():
+                for subkey, subvalue in value.items():
+                    if key == "node_features_dict":
+                        job_shop_data[subkey].x = torch.from_numpy(subvalue)
+                    elif key == "edge_index_dict":
+                        job_shop_data[subkey].edge_index = torch.from_numpy(
+                            subvalue
+                        )
+            job_shop_data["y"] = torch.tensor(
+                list(action_probs.values()), dtype=torch.float32
+            )
+            job_shop_data["valid_pairs"] = torch.tensor(
+                list(action_probs.keys())
+            )
+            hetero_dataset.append(job_shop_data)
+        return hetero_dataset
+
+    @staticmethod
+    def get_observation_action_pairs(
+        schedules_json: list[dict],
+        feature_observers_types: Sequence[
+            str
+            | FeatureObserverType
+            | Type[FeatureObserver]
+            | FeatureObserverConfig,
+        ],
+    ) -> tuple[
+        list[ResourceTaskGraphObservationDict],
+        list[dict[tuple[int, int, int], float]],
+    ]:
+        observations: list[ResourceTaskGraphObservationDict] = []
+        action_probabilities_sequence: list[
+            dict[tuple[int, int, int], float]
+        ] = []
+        for schedule_dict in tqdm.tqdm(
+            schedules_json, desc="Processing schedules"
+        ):
+            schedule = Schedule.from_dict(**schedule_dict)
+            graph = build_resource_task_graph(schedule.instance)
+            env = SingleJobShopGraphEnv(
+                graph,
+                feature_observer_configs=feature_observers_types,
+                ready_operations_filter=None,
+            )
+            wrapped_env = ResourceTaskGraphObservation(env)
+            optimal_ops_observer = OptimalOperationsObserver(
+                wrapped_env.unwrapped.dispatcher, schedule
+            )
+            obs, info = wrapped_env.reset()
+            done = False
+            while not done:
+
+                action_probs = get_optimal_actions(
+                    optimal_ops_observer,
+                    JobShopDataset.map_available_ops_ids_to_original(
+                        info["available_operations_with_ids"],
+                        obs["original_ids_dict"],
+                    ),
+                )
+                if len(action_probs) > 1:
+                    obs["node_features_dict"] = (
+                        JobShopDataset.normalize_features(
+                            obs["node_features_dict"]
+                        )
+                    )
+                    observations.append(obs)
+                    action_probs_adjusted: dict[
+                        tuple[int, int, int], float
+                    ] = {}
+                    assert len(info["available_operations_with_ids"]) == len(
+                        action_probs
+                    )
+                    for key, value in zip(
+                        info["available_operations_with_ids"],
+                        action_probs.values(),
+                    ):
+                        action_probs_adjusted[key] = value
+                    action_probabilities_sequence.append(action_probs_adjusted)
+                optimal_actions = [
+                    action
+                    for action, value in action_probs.items()
+                    if value == 1.0
+                ]
+                action_choice = random.choice(optimal_actions)
+                _, machine_id, job_id = action_choice
+                obs, _, done, _, info = wrapped_env.step((job_id, machine_id))
+        return observations, action_probabilities_sequence
+
+    @staticmethod
+    def map_available_ops_ids_to_original(
+        available_operations_with_ids: list[tuple[int, int, int]],
+        original_ids: dict[str, np.ndarray],
+    ) -> list[tuple[int, int, int]]:
+        new_ids = []
+        for operation_id, machine_id, job_id in available_operations_with_ids:
+            original_operation_id = original_ids["operation"][operation_id]
+            original_machine_id = original_ids["machine"][machine_id]
+            new_ids.append(
+                (original_operation_id, original_machine_id, job_id)
+            )
+        return new_ids
+
+    @staticmethod
+    def normalize_features(
+        node_features_dict: dict[str, np.ndarray],
+        indices_to_normalize: dict[str, Sequence[int]] | None = None,
+    ) -> dict[str, np.ndarray]:
+        if indices_to_normalize is None:
+            indices_to_normalize = {
+                "operation": list(range(8)),
+                "machine": list(range(4)),
+            }
+        for key, indices in indices_to_normalize.items():
+            # Divide by the maximum value checking for division by zero
+            max_values = np.max(node_features_dict[key], axis=0)
+            max_values[max_values == 0] = 1
+            node_features_dict[key][:, indices] /= max_values[indices]
+
+        return node_features_dict
--- scripts/_create_graph_data.py
+++ scripts/_create_graph_data.py
@@ -1,6 +1,7 @@
-import torch
+from gnn_scheduler.data import JobShopDataset
 
-print(f"PyTorch version: {torch.__version__}")
-print(f"CUDA available: {torch.cuda.is_available()}")
-if torch.cuda.is_available():
-    print(f"CUDA device: {torch.cuda.get_device_name(0)}")
+
+if __name__ == "__main__":
+    dataset = JobShopDataset()
+    dataset.process()
+    print("Done")
