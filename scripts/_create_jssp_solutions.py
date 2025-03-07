0eb05d1a8c9c97ce783391c7e2d026acbe4cb9b3
Pabloo22 <pablete.arino@gmail.com>
2025-03-07 19:52:41 +0100

    Add train json

--- scripts/_create_jssp_solutions.py
+++ scripts/_create_jssp_solutions.py
@@ -10,12 +10,13 @@ def _main(
     file_name: str = "small_random_instances_0.json",
     num_jobs: int | tuple[int, int] = (3, 6),
     num_machines: int | tuple[int, int] = (3, 5),
+    seed: int = 0,
 ):
     generator = GeneralInstanceGenerator(
         num_jobs=num_jobs,
         num_machines=num_machines,
         allow_less_jobs_than_machines=False,
-        seed=0,
+        seed=seed,
         name_suffix="small_random_instance",
         iteration_limit=n_instances,
     )
@@ -26,7 +27,9 @@ def _main(
             schedule = solver(instance)
             schedules.append(schedule.to_dict())
     finally:
-        with open(get_data_path() / file_name, "w", encoding="utf-8") as f:
+        with open(
+            get_data_path() / "raw" / file_name, "w", encoding="utf-8"
+        ) as f:
             json.dump(schedules, f, indent=None, separators=(",", ":"))
 
 
@@ -35,15 +38,23 @@ if __name__ == "__main__":
     # _main(100_000)
 
     # Evaluation instances
+    # _main(
+    #     1_000,
+    #     "instances10x10_eval_0.json",
+    #     num_jobs=(10, 10),
+    #     num_machines=(10, 10),
+    # )
+    # _main(
+    #     1_000,
+    #     "instances5x5_eval_0.json",
+    #     num_jobs=(5, 5),
+    #     num_machines=(5, 5),
+    # )
+
     _main(
-        1_000,
-        "instances10x10_eval_0.json",
+        25_000,
+        "instances10x10_train_1.json",
         num_jobs=(10, 10),
         num_machines=(10, 10),
-    )
-    _main(
-        1_000,
-        "instances5x5_eval_0.json",
-        num_jobs=(5, 5),
-        num_machines=(5, 5),
+        seed=1,
     )
