d717da82bc1cbf269022ba6da7d3f168bea7f952
pabloo22 <pablete.arino@gmail.com>
2025-03-15 20:59:47 +0100

    Split train json files

---
+++ scripts/_divide_json.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from gnn_scheduler.utils import get_data_path
+
+
+def divide_data_list(data_list, num_splits):
+    split_size = len(data_list) // num_splits
+    splits = []
+    for i in range(num_splits):
+        splits.append(data_list[i * split_size : (i + 1) * split_size])
+    return splits
+
+
+def _main(json_filename: str, num_splits=2):
+    json_path = str(get_data_path() / "raw")
+    json_filename = os.path.join(json_path, json_filename)
+    with open(json_filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    splits = divide_data_list(data, num_splits)
+    json_filename_stem = json_filename.split(".")[0]
+    for i, split in enumerate(splits):
+        with open(
+            f"{json_filename_stem}_{i}.json", "w", encoding="utf-8"
+        ) as f:
+            json.dump(split, f)
+
+    # Remove the original file
+    os.remove(json_filename)
+
+
+if __name__ == "__main__":
+    from gnn_scheduler.configs.experiment_configs import TRAIN_JSONS
+
+    for train_json in TRAIN_JSONS:
+        _main(train_json)
