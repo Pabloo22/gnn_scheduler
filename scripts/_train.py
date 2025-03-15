68de410041ffc46d0294b8500227b996f2396544
Pabloo22 <pablete.arino@gmail.com>
2025-03-15 15:07:01 +0100

    Refactor training script to use DatasetManager for data loading and integrate wandb for performance logging

--- scripts/_train.py
+++ scripts/_train.py
@@ -1,10 +1,17 @@
+from functools import partial
+import os
+
+import wandb
 import torch
 from torch_geometric.loader import DataLoader
-from gnn_scheduler.model import ResidualSchedulingGNN
-from gnn_scheduler.data import JobShopDataset
+from job_shop_lib.benchmarking import load_all_benchmark_instances
 
+from gnn_scheduler.model import ResidualSchedulingGNN
+from gnn_scheduler.data import JobShopDataset, DatasetManager
+from gnn_scheduler.eval import get_performance_dataframe
 from gnn_scheduler.trainer import Trainer
 from gnn_scheduler.configs import Config
+from gnn_scheduler.solve_jssp import solve_job_shop_with_gnn
 
 
 def _main(config: Config):
@@ -18,22 +25,14 @@ def _main(config: Config):
     #     processed_filenames_prefix="instances_train10x10",
     #     raw_filenames=config.train_jsons,
     # )
-    dataset_manager_train = JobShopDataset(
+    dataset_manager_train = DatasetManager(
         raw_filenames=config.train_jsons,
-        processed_filenames_prefix=config.processed_filenames_prefix_train,
-        num_chunks=config.num_chunks_train,
-        max_chunks_in_memory=config.max_chunks_in_memory,
+        dataloader_kwargs={"batch_size": config.batch_size, "shuffle": True},
     )
     val_dataset_10x10 = JobShopDataset(
-        num_chunks=1,
-        raw_filenames="instances10x10_eval_0.json",
-        processed_filenames_prefix="instances10x10_eval",
+        raw_filename="instances10x10_eval_0.json"
     )
-    val_dataset_5x5 = JobShopDataset(
-        num_chunks=1,
-        raw_filenames="instances5x5_eval_0.json",
-        processed_filenames_prefix="instances5x5_eval",
-    )
+    val_dataset_5x5 = JobShopDataset(raw_filename="instances5x5_eval_0.json")
     val_dataloader_10x10 = DataLoader(
         val_dataset_10x10, batch_size=config.batch_size
     )
@@ -45,42 +44,52 @@ def _main(config: Config):
 
     # Create loss function
     criterion = torch.nn.BCEWithLogitsLoss()
-    for _ in range(config.epochs):
-        for train_dataset in dataset_manager_train:
-            train_dataloader = DataLoader(
-                train_dataset, batch_size=config.batch_size, shuffle=True
-            )
+    trainer = Trainer(
+        model=model,
+        train_dataloader=dataset_manager_train,
+        val_dataloaders={
+            "instances10x10_eval": val_dataloader_10x10,
+            "instances5x5_eval": val_dataloader_5x5,
+        },
+        optimizer=optimizer,
+        criterion=criterion,
+        primary_val_key=config.primary_val_key,
+        epochs=config.epochs,
+        metrics=config.metrics,
+        grad_clip_val=config.grad_clip_val,
+        early_stopping_patience=config.early_stopping_patience,
+        experiment_name=config.experiment_name,
+    )
 
-            # Set up learning rate scheduler
-            # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            #     optimizer, mode="max", factor=0.5, patience=5
-            # )
+    # Train the model
+    trainer.train()
+    best_model = trainer.model
+    gnn_solver = partial(
+        solve_job_shop_with_gnn,
+        model=best_model,
+    )
+    # Evaluate the model
+    performance_df = get_performance_dataframe(
+        gnn_solver,
+        load_all_benchmark_instances().values(),
+    )
+    print(performance_df)
 
-            # Initialize trainer with multiple metrics
-            trainer = Trainer(
-                model=model,
-                train_dataloader=train_dataloader,
-                val_dataloaders={
-                    "instances10x10_eval": val_dataloader_10x10,
-                    "instances5x5_eval": val_dataloader_5x5,
-                },
-                optimizer=optimizer,
-                criterion=criterion,
-                primary_val_key=config.primary_val_key,
-                epochs=1,
-                metrics=config.metrics,
-                grad_clip_val=config.grad_clip_val,
-                early_stopping_patience=config.early_stopping_patience,
-                experiment_name=config.experiment_name,
-            )
+    # Save .csv file with results
+    performance_df.to_csv(
+        os.path.join(trainer.checkpoint_dir, "best_model_performance.csv"),
+        index=False,
+    )
 
-            # Train the model
-            trainer.train()
+    # save performance metrics to wandb
+    wandb.log({"performance_metrics": performance_df})
+
+    wandb.finish()
 
 
 if __name__ == "__main__":
     from gnn_scheduler.configs.experiment_configs import (
-        EXPERIMENT_3,
+        TESTING_CONFIG,
     )
 
-    _main(EXPERIMENT_3)
+    _main(TESTING_CONFIG)
