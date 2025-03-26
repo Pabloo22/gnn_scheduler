5ae36c0c44e75171ef772d9984181bb48a45d9b4
Pabloo22 <pablete.arino@gmail.com>
2025-03-26 17:43:03 +0100

    Add test script for store_each_n_steps functionality

---
+++ scripts/_test_store_each_n_steps_dist.py
@@ -0,0 +1,13 @@
+
+
+n_elements = 10 * 5
+visit_each_n_steps = 33
+counter_of_visits = [0] * n_elements
+i = 0
+for _ in range(12500):
+    for j in range(n_elements):
+        if i % visit_each_n_steps == 0:
+            counter_of_visits[j] += 1
+        i += 1
+
+print(counter_of_visits)
