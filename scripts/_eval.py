9d9c16245bd8bad8ec0d82cf0977b711e9a28201
pabloo22 <pablete.arino@gmail.com>
2025-03-28 10:14:08 +0100

    Add results

--- scripts/_eval.py
+++ scripts/_eval.py
@@ -120,4 +120,4 @@ def create_optimality_gap_plot(
 
 
 if __name__ == "__main__":
-    main("experiment16_results", only_taillard=True, model_name="HGIN")
+    main("experiment22_results", only_taillard=True, model_name="HGIN")
